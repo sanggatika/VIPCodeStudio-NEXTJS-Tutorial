3322d2bd14cd84d68c1176dae83d9a51a0add375
sanggatika <sanggatika@gmail.com>
2024-04-02 14:35:23 +0700

    utorial Next JS Bahasa Indonesia Pages Router : 5. Custom Error Page & Document

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import styles from '@/styles/404.module.scss';
+
+const Custome404Page = () => {
+    return (
+        <div className={styles.error}>
+            <img src="/not_found.png" alt="404" className={styles.error__image} />
+            <div>
+                Halaman Tidak Ditemukan
+            </div>
+        </div>  
+    );
+}
+
+export default Custome404Page;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head"
+
 export default function Home() {
   return (
     <div>
+      <Head>  
+        <title>My NEXTJS 13.4.7 App - Home</title>
+      </Head>
       <div>
         Hello Word !..
       </div>
