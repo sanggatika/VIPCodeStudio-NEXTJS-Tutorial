4f2e7ce8b137e7fc19edf6903f5851dbdfd741d8
sanggatika <sanggatika@gmail.com>
2024-04-02 10:12:45 +0700

    Tutorial Next JS Bahasa Indonesia Pages Router : 4. Styling

---
+++ src/views/Auth/Login/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import styles from "./Login.module.scss";
+
+const LoginViews = () => {
+    const { push } = useRouter();
+    const handleLogin = () => {
+        push("/product");
+    };
+
+    return (
+        <div className={styles.login}>
+            <h1 className="text-3xl">Login Page</h1>
+            <button onClick={() => handleLogin()}>Login</button>
+            <p style={{ color: "red", border: "1px solid red", borderRadius: "10px", padding: "10px", marginTop: "10px" }}> 
+                Belum punya akun ? Registrasi <Link href={"/auth/register"}> Disini </Link>
+            </p>            
+        </div>
+    )
+}
+
+export default LoginViews
