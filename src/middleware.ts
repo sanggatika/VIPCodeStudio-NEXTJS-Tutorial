b8c9c466cabc205abff01ddc8db62aab3edadb16
sanggatika <sanggatika@gmail.com>
2024-04-04 10:18:43 +0700

    Tutorial Next JS Bahasa Indonesia Pages Router : 13. Setup Next-Auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import withAuth from "./middlewares/withAuth";
 
-export function middleware(request: NextRequest) {
+export function mainMiddleware(request: NextRequest) {
     // return NextResponse.redirect(new URL("/", request.url));
     // return NextResponse.next();
 
-    const isLogin = true;
-    if(!isLogin) {
-        return NextResponse.next();
-    }else {
-        return NextResponse.redirect(new URL("/auth/login", request.url));
-    }
+    // const isLogin = true;
+    // if(!isLogin) {
+    //     return NextResponse.next();
+    // }else {
+    //     return NextResponse.redirect(new URL("/auth/login", request.url));
+    // }
+
+    const res = NextResponse.next();
+    return res;
 }
 
-export const config = {
-    matcher: ["/product/:path*", "/about"],
-}
+export default withAuth(mainMiddleware, ["/profile"]);
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,61 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+              username: { label: "Username", type: "text"},
+              password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+              // Add logic here to look up the user from the credentials supplied
+              const { username, password } = credentials as { username: string, password: string };
+              
+              const user = { id: "1", name: "J Smith", email: username, password: password };
+        
+              if (user) {
+                // Any object returned will be saved in `user` property of the JWT
+                return user
+              } else {
+                // If you return null then an error will be displayed advising the user to check their details.
+                return null
+        
+                // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+              }
+            }
+        })
+    ],
+    callbacks: {
+        jwt({ token, account, profile, user }) {
+            if (account?.provider === "credentials") {
+                token.email = user.email;
+                token.fullname = user.name;
+            }
+            return token;
+        },
+    
+        session({ session, token }: any) {
+            if ("email" in token) {
+                session.user.email = token.email;
+            }
+            if ("fullname" in token) {
+                session.user.fullname = token.fullname;
+            }
+            return session;
+        }        
+    }
+}
+
+export default NextAuth(authOptions);
